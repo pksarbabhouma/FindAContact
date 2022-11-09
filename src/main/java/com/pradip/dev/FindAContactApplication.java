1f6b3b300c27dd2311038920371a91a98dada298
pksarbabhouma <contact.pradipks@gmail.com>
2022-11-09 07:29:09 +0530

    Initial maven project created from spring starter io

---
+++ src/main/java/com/pradip/dev/FindAContactApplication.java
@@ -0,0 +1,13 @@
+package com.pradip.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindAContactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindAContactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pradip/dev/FindAContactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pradip.dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindAContactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
